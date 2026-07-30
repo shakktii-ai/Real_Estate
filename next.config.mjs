6c017f513abf50fb5e3793ba8794dc2b63df589d
syncsoftech <shakktii.ai@gmail.com>
2026-07-30 15:55:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbopack: {
+      root: '.',
+    },
+  },
   images: {
     remotePatterns: [
       {
